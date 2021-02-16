6d8557a9a7346ad82556f052f2f9335375ed9970
91aidenlee <91aiden.lee@gmail.com>
2021-02-16 21:29:16 +0900

    2021-02-16 민호(풀이 수정)

--- 0216/minho/가운데글자가져오기.js
+++ 0216/minho/가운데글자가져오기.js
@@ -1,13 +1,15 @@
 function solution(s) {
   let answer = "";
   const wordLength = s.length;
+  const midIndex = Math.floor(wordLength / 2);
+  // 전체 길이를 나눠서 내림하면 중간 인덱스를 구할 수 있음
   if (wordLength % 2 === 1) {
     // 단어의 길이가 홀수 일 경우
-    const midIndex = Math.floor(wordLength / 2); // 전체 길이를 나눠서 내림하면 중간 인덱스를 구할 수 있음..
     answer = s[midIndex];
   } else {
     // 단어의 길이가 짝수 일 경우
-    answer = s.substring(1, s.length - 1);
+    answer = s.substring(midIndex - 1, midIndex + 1);
+    // substring(startIndex, endIndex) 마지막 인덱스는 포함하지 않음
   }
   return answer;
 }
--- 0216/minho/수박수박수.js
+++ 0216/minho/수박수박수.js
@@ -1,8 +1,8 @@
 function solution(n) {
   let answer = "";
+  const su = "수";
+  const bak = "박";
   for (let i = 1; i <= n; i++) {
-    const su = "수";
-    const bak = "박";
     if (i % 2 === 1) answer += su;
     else answer += bak;
   }
