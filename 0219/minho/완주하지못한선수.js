491a8c571b548c1adc353f0f38b9c9652c544f04
91aidenlee <91aiden.lee@gmail.com>
2021-02-19 09:35:28 +0900

    2021-02-19 민호(수정)

--- 0219/minho/완주하지못한선수.js
+++ 0219/minho/완주하지못한선수.js
@@ -2,7 +2,7 @@ function solution(participant, completion) {
   let answer = "";
   participant.sort();
   completion.sort();
-  for (let i = 0; i <= participant.length - 1; i++) {
+  for (let i = 0; i < participant.length; i++) {
     if (participant[i] !== completion[i] || completion[i] === undefined) {
       // 참가자의 마지막 인덱스에 탈락자가 있을 경우
       answer = participant[i];
