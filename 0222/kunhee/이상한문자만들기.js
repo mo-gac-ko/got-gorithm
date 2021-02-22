711196480f01d99f491c643f84ca17230ca71e01
p-jaeeun <j_brick@naver.com>
2021-02-22 10:02:15 +0900

    2021-02-22 재은

---
+++ 0222/kunhee/이상한문자만들기.js
@@ -0,0 +1,16 @@
+// 못 풀었음...
+
+function solution(s) {
+  var answer = "";
+  let words = s.split(" ");
+  answer = words.split("").map((item, index) => {
+    //여기서 꼬인듯
+    if (index % 2 == 0) {
+      return item.toUpperCase();
+    } else {
+      return item.toLowerCase();
+    }
+  });
+  console.log(answer);
+  return answer;
+}
