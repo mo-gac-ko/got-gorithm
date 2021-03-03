36848e297e5e9dc8e254f7f8e6bfc5601697f8e5
DESKTOP-I8S375Q\kunhee <tha920@naver.com>
2021-03-03 11:00:52 +0900

    2021-03-03 근희(카카오문제 품)

--- 0303/kunhee/다트게임.js
+++ 0303/kunhee/다트게임.js
@@ -2,10 +2,18 @@ function solution(dartResult) {
   var answer = 0;
   let tmp = dartResult.split("");
 
+  /**
+   * 한글자씩 체크할때 슬 변수
+   */
   let answerChar = "";
+
+  /**
+   * 숫자를 계산할때 쓸 배열
+   */
   let answerArr = [];
 
-  answer = tmp.map((item, idx) => {
+  tmp.map((item, idx) => {
+    //글자가 숫자가 아닐때
     if (isNaN(item)) {
       if (item === "S") {
         answerArr.push(answerChar * 1);
@@ -15,14 +23,24 @@ function solution(dartResult) {
         answerArr.push(answerChar * answerChar * answerChar);
       }
       if (item === "*") {
+        answerArr[answerArr.length - 1] = answerArr[answerArr.length - 1] * 2;
+        if (answerArr[answerArr.length - 2]) {
+          answerArr[answerArr.length - 2] = answerArr[answerArr.length - 2] * 2;
+        }
+      }
+      if (item === "#") {
+        answerArr[answerArr.length - 1] = answerArr[answerArr.length - 1] * -1;
       }
 
+      //한글자마다 체크하고 넘어갈때 초기화시킨다.
       answerChar = "";
     } else {
+      //숫자일때 answerChar에다가 모은다.
       answerChar += item;
     }
   });
-  console.log(answerArr);
+  //다 계산된 숫자들이 담긴 배열을 합산한다.
+  answer = answerArr.reduce((acc, crr) => acc + crr, 0);
 
   return answer;
 }
