8aed5419ac9697c78a5a1b6eb70a06f16da972ba
p-jaeeun <j_brick@naver.com>
2021-03-08 12:55:24 +0900

    2021-03-08 재은(수정)

--- 0308/jaeeun/비밀지도.js
+++ 0308/jaeeun/비밀지도.js
@@ -3,15 +3,22 @@ function solution(n, arr1, arr2) {
   let convert1 = arr1
     .map((num) => num.toString(2))
     .map((num) => {
-      return num.length === n ? num : "0" * (n - num.length) + num;
+      return num.length < n ? "0".repeat(n - num.length) + num : num;
     });
   let convert2 = arr2
     .map((num) => num.toString(2))
     .map((num) => {
-      return num.length === n ? num : "0" * (n - num.length) + num;
+      return num.length < n ? "0".repeat(n - num.length) + num : num;
     });
-  // console.log(convert1);
-  // console.log(convert2);
+
+  for (let i = 0; i < n; i++) {
+    let result = "";
+    for (let j = 0; j < n; j++) {
+      if (convert1[i][j] === "1" || convert2[i][j] === "1") result += "#";
+      else result += " ";
+    }
+    answer.push(result);
+  }
 
   return answer;
 }
