9f52608baa19a9e756b7fe86453433766eccf1ae
91aidenlee <91aiden.lee@gmail.com>
2021-03-08 10:34:19 +0900

    2021-03-08 민호(수정)

--- 0308/minho/비밀지도.js
+++ 0308/minho/비밀지도.js
@@ -3,13 +3,22 @@ function solution(n, arr1, arr2) {
 
   const toBinaryArr1 = arr1.map((idx) => {
     const binaryNum = idx.toString(2);
-    binaryNum.length < n ? "0".repeat(n - binaryNum.length) + binaryNum : binaryNum;
+    return binaryNum.length < n ? "0".repeat(n - binaryNum.length) + binaryNum : binaryNum;
   });
 
   const toBinaryArr2 = arr2.map((idx) => {
     const binaryNum = idx.toString(2);
-    binaryNum.length < n ? "0".repeat(n - binaryNum.length) + binaryNum : binaryNum;
+    return binaryNum.length < n ? "0".repeat(n - binaryNum.length) + binaryNum : binaryNum;
   });
 
+  for (let i = 0; i < n; i++) {
+    let code = "";
+    for (let j = 0; j < n; j++) {
+      if (toBinaryArr1[i][j] === "1" || toBinaryArr2[i][j] === "1") {
+        code += "#";
+      } else code += " ";
+    }
+    answer.push(code);
+  }
   return answer;
 }
