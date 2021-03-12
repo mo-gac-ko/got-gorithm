fd6f5a0ee81c0534d72272d88cdc9a857a2e9268
DESKTOP-I8S375Q\kunhee <tha920@naver.com>
2021-03-12 10:01:35 +0900

    2021-03-12 근희

---
+++ 0312/kunhee/카펫.js
@@ -0,0 +1,40 @@
+function solution(brown, yellow) {
+  var answer = [];
+
+  let tmpAllBoxes = brown + yellow;
+
+  let arr = [];
+  for (var i = 2; i < tmpAllBoxes; i++) {
+    if (tmpAllBoxes % i == 0) {
+      arr.push(i);
+    }
+  }
+
+  arr.map((item, index) => {
+    if (item * item === tmpAllBoxes) {
+      answer.push(item, item);
+    }
+  });
+
+  if (answer.length !== 0) {
+    return answer;
+  }
+
+  arr.map((item, index) => {
+    if (index + 1 <= arr.length) {
+      if (item * arr[index + 1] === tmpAllBoxes) {
+        if ((arr[index + 1] - 2) * (item - 2) === yellow) {
+          answer.push(arr[index + 1], item);
+        }
+      }
+    }
+  });
+  if (answer.length !== 0) {
+    return answer;
+  } else {
+    answer.push(yellow + 2, 3);
+  }
+  console.log(answer);
+
+  return answer;
+}
