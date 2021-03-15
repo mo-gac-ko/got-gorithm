b8d91f81ce648be88757fc8f65e96fce49b56c9c
p-jaeeun <j_brick@naver.com>
2021-03-15 23:01:54 +0900

    2021-03-15 재은(수정)

--- 0315/jaeeun/오픈채팅방.js
+++ 0315/jaeeun/오픈채팅방.js
@@ -1,18 +1,24 @@
 function solution(record) {
   let result = [];
-  let actions = ["님이 들어왔습니다.", "님이 나갔습니다"];
-  record.map((i) => {
-    let log = i.split(" ");
-    if (log[0] === "Enter") {
-      let nickname = log[2];
-      result.push(nickname + actions[0]);
-    } else if (log[0] === "leave") {
-      result.push(log[2] + actions[1]);
-    } else log[0] === "change";
-    let nickname = log[2];
-    result.push(nickname + actions[0]);
-  });
+  let nickname = {}; // 아이디와 닉네임 매치하기 위한 객체생성
+
+  // 로그 배열 생성
+  let log = record.map((el) => el.split(" "));
+
+  // 아이디와 닉네임 매치 (leave인 경우 undefined 예외처리!)
+  for (let i = 0; i < log.length; i++) {
+    if (log[i].length === 3) {
+      nickname[log[i][1]] = log[i][2];
+    }
+  }
+
+  // 값 출력
+  for (let i = 0; i < log.length; i++) {
+    if (log[i][0] === "Enter") {
+      result.push(nickname[log[i][1]] + "님이 들어왔습니다.");
+    } else if (log[i][0] === "Leave") {
+      result.push(nickname[log[i][1]] + "님이 나갔습니다.");
+    }
+  }
   return result;
 }
-
-//못품
