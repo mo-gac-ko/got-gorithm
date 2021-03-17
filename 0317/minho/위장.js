4d40c1d5e5616afa592b99f01c922a87b01092c6
91aidenlee <91aiden.lee@gmail.com>
2021-03-17 21:41:38 +0900

    2021-03-17 민호

---
+++ 0317/minho/위장.js
@@ -0,0 +1,18 @@
+function solution(clothes) {
+  let answer = 0;
+  let options = {};
+
+  // 옷의 종류별로 갯수 카운팅
+  clothes.forEach((cloth) => {
+    if (options[cloth[1]]) {
+      // 객체에 키가 존재하면 + 1
+      options[cloth[1]]++;
+    } else {
+      // 객체에 키가 존재하지 않는다면 해당 키의 값을 1로 설정
+      options[cloth[1]] = 1;
+    }
+  });
+  console.log(options);
+
+  return answer;
+}
