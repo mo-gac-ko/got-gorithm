69a9a4d4091583d466b1f56def465d4a01437763
DESKTOP-I8S375Q\kunhee <tha920@naver.com>
2021-03-18 21:19:23 +0900

    2021-03-18 근희

--- 0317/kunhee/위장.js
+++ 0317/kunhee/위장.js
@@ -12,3 +12,16 @@ function solution(clothes) {
   }
   return answer - 1;
 }
+function solution(clothes) {
+  var answer = 1;
+  let count = {};
+  clothes.map((item) => {
+    if (item[1] in count) {
+      count[item[1]] += 1;
+    } else count[item[1]] = 1;
+  });
+  for (let i in count) {
+    answer *= count[i] + 1;
+  }
+  return answer - 1;
+}
