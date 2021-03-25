691a0fcc1d8eabf94016aa48b319d26052407688
DESKTOP-I8S375Q\kunhee <tha920@naver.com>
2021-03-25 22:28:44 +0900

    2021-03-25 근희(수정)

--- 0325/kunhee/소수찾기.js
+++ 0325/kunhee/소수찾기.js
@@ -17,15 +17,21 @@ function solution(n) {
 
 //찾아보니 에라토스테네스의 체 라는 것을 이해해야 하는 수학적 문제이다.
 //소수를 구하기보다는 소수가 아닌 것을 제외하면 되고 그건 소수들의 배수를 제거 하면 된다.
+// **연산자는 거듭제곱
 function solution(n) {
-  const 소수들 = new Array(n).fill(true);
-  소수들[0] = false;
-  for (let i = 2; i ** 2 <= n; i++) {
-    if (소수들[i - 1] === true) {
-      for (let j = i ** 2; j <= n; j += i) {
-        소수들[j - 1] = false;
+  const s = new Set();
+  //짝수는 소수 일수가 없음
+  for (let i = 3; i <= n; i += 2) {
+    s.add(i);
+  }
+  s.add(2);
+
+  for (let j = 3; j ** 2 < n; j++) {
+    if (s.has(j)) {
+      for (let k = j ** 2; k <= n; k += j) {
+        s.delete(k);
       }
     }
   }
-  return 소수들.filter((e) => e).length;
+  return s.size;
 }
